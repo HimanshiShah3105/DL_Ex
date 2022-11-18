f0f76a160c641394e24a348da6fe2adef7d32cd6
HimanshiShah3105 <himanshi3105@gmail.com>
2022-11-18 12:58:03 +0100

    first

---
+++ Optimization/Optimizers.py
@@ -0,0 +1,9 @@
+class Sgd():
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+        
+    def calculate_update(self, weight_tensor, gradient_tensor):
+        self.weight_tensor = weight_tensor
+        self.gradient_tensor = gradient_tensor
+        weight_update = self.weight_tensor - self.learning_rate * self.gradient_tensor
+        return weight_update
